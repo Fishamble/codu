aed148f568b0b7f44e1580377372ff310115ab37
John Paul Larkin <107807466+John-Paul-Larkin@users.noreply.github.com>
2024-11-18 09:57:10 +0000

    revert migration

--- drizzle/0011_years-experience-user-table.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "session" ADD PRIMARY KEY ("sessionToken");--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "yearsOfExperience" text;--> statement-breakpoint
-ALTER TABLE "user" ADD COLUMN "onboardingComplete" timestamp with time zone;
